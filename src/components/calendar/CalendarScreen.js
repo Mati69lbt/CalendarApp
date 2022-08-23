880a158ce72cae162695c096ab6b1857c2b10026
Mati69lbt <Mati69_lbt@hotmail.com>
2022-08-23 11:00:14 -0300

    importacion de calendario

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,31 @@
 import React from "react";
 import Navbar from "../ui/Navbar";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment);
+
+const events = [
+  {
+    title: "Cumpleaños de Matias",
+    start: moment().toDate(),
+    end: moment().add(2, "hours").toDate(),
+    bgcolor: "#fafafa",
+  },
+];
 
 const CalendarScreen = () => {
   return (
     <div>
       <Navbar />
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
     </div>
   );
 };
