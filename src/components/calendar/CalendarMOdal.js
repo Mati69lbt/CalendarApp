6b9a86e38c9b8731df1c8cb8bbb67d3f38c570c1
Mati69lbt <Mati69_lbt@hotmail.com>
2022-08-24 09:07:10 -0300

    Formulario Modal

--- src/components/calendar/CalendarMOdal.js
+++ src/components/calendar/CalendarMOdal.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
+import DateTimePicker from "react-datetime-picker";
+import moment from "moment";
 
 const customStyles = {
   content: {
@@ -13,16 +15,27 @@ const customStyles = {
 };
 Modal.setAppElement("#root");
 
+const now = moment().minutes(0).seconds(0).add(1, "hours");
+const end = moment().minutes(0).seconds(0).add(2, "hours");
+
 const CalendarMOdal = () => {
-  const [isOpen, setIsOpen] = useState(true);
-  function closeModal() {
-    setIsOpen(false);
-  }
+  const [dateStart, setDateStart] = useState(now.toDate());
+  const [dateEnd, setDateEnd] = useState(end.toDate());
+
+  const closeModal = () => {};
+
+  const handleStartDateChange = (e) => {
+    setDateStart(e);
+    console.log(`hora inicio: ${e}`);
+  };
+  const handleEndDateChange = (e) => {
+    setDateEnd(e);
+    console.log(`hora fin: ${e}`);
+  };
 
   return (
     <Modal
-      isOpen={isOpen}
-      // onAfterOpen={afterOpenModal}
+      isOpen={true}
       onRequestClose={closeModal}
       style={customStyles}
       closeTimeoutMS={200}
@@ -30,9 +43,61 @@ const CalendarMOdal = () => {
       className="modal"
       overlayClassName="modal-fondo"
     >
-      <h1>Hola a todos!</h1>
+      <h1> Nuevo evento </h1>
       <hr />
-      <span>Hola putic!</span>
+      <form className="container">
+        <div className="form-group mb-2">
+          <label>Fecha y hora inicio</label>
+          <DateTimePicker
+            onChange={handleStartDateChange}
+            value={dateStart}
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group mb-2">
+          <label>Fecha y hora fin</label>
+          <DateTimePicker
+            onChange={handleEndDateChange}
+            value={dateEnd}
+            minDate={dateStart}
+            className="form-control"
+          />
+        </div>
+
+        <hr />
+        <div className="form-group mb-2">
+          <label>Titulo y notas</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Título del evento"
+            name="title"
+            autoComplete="off"
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            Una descripción corta
+          </small>
+        </div>
+
+        <div className="form-group mb-2">
+          <textarea
+            type="text"
+            className="form-control"
+            placeholder="Notas"
+            rows="5"
+            name="notes"
+          ></textarea>
+          <small id="emailHelp" className="form-text text-muted">
+            Información adicional
+          </small>
+        </div>
+
+        <button type="submit" className="btn btn-outline-primary btn-block">
+          <i className="far fa-save"></i>
+          <span> Guardar</span>
+        </button>
+      </form>
     </Modal>
   );
 };
